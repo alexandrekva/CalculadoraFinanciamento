83a8b26fcb32c50eb591a9b27ad6a3e7c2581108
Alexandre Alves <alexandre.alves@ucsal.edu.br>
2024-01-08 23:28:33 -0300

    Initial commit

---
+++ app/src/main/java/com/alexkva/calculadorafinanciamento/CalculadoraFinanciamentoApplication.kt
@@ -0,0 +1,7 @@
+package com.alexkva.calculadorafinanciamento
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CalculadoraFinanciamentoApplication: Application()
