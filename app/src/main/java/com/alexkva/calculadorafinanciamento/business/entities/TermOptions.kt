eb8d7997e5f5c39420e09dbb5e19f7100d6448ca
Alexandre Alves <alexandre.alves@ucsal.edu.br>
2024-01-18 02:19:40 -0300

    warnings resolved

--- app/src/main/java/com/alexkva/calculadorafinanciamento/business/entities/TermOptions.kt
+++ app/src/main/java/com/alexkva/calculadorafinanciamento/business/entities/TermOptions.kt
@@ -1,7 +1,5 @@
 package com.alexkva.calculadorafinanciamento.business.entities
 
-import androidx.compose.runtime.Immutable
-
 sealed class TermOptions(val label: String, val charLimit: Int) {
     object Months: TermOptions(label = "meses", charLimit = 3)
     object Years: TermOptions(label = "anos", charLimit = 2)
--- app/src/main/java/com/alexkva/calculadorafinanciamento/ui/components/LabeledSwitch.kt
+++ app/src/main/java/com/alexkva/calculadorafinanciamento/ui/components/LabeledSwitch.kt
@@ -4,10 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -19,7 +15,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun LabeledSwitch(
